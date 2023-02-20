09f10132be56e2254d336763d65ed962a4518efc
Adriana <jesus.caro15@gmail.com>
2023-02-20 13:08:26 -0300

    proyecto cv inti

---
+++ javascript/funciones.js
@@ -0,0 +1,14 @@
+document.getElementById('castellano').onclick = function(){
+   
+   document.getElementById('demo_castellano').innerHTML = "Categoria: Nativo"; 
+}
+
+document.getElementById('ingles').onclick = function(){
+   
+    document.getElementById('demo_ingles').innerHTML = "Categoria: B2"; 
+ }
+
+ document.getElementById('portugues').onclick = function(){
+   
+    document.getElementById('demo_portugues').innerHTML = "Categoria: B1"; 
+ } 
